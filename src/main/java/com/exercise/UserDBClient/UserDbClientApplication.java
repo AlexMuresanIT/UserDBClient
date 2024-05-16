0b84d800ceb3c08020a8ed8896cd00eacbe46cd9
Alex Muresan <alexmuresan55@yahoo.com>
2024-05-16 14:44:07 +0300

    commented all in main

--- src/main/java/com/exercise/UserDBClient/UserDbClientApplication.java
+++ src/main/java/com/exercise/UserDBClient/UserDbClientApplication.java
@@ -16,27 +16,27 @@ public class UserDbClientApplication {
 		SpringApplication.run(UserDbClientApplication.class, args);
 	}
 
-	@Bean
-	ApplicationRunner applicationRunner() {
-		return args -> {
-			UserRESTClient client = new UserRESTClient();
-			client.createUser(new User(1L,"Alex","alex@gmail.com","pw","Medias"));
-			client.getUserById(1L);
-			client.getUserById(1L);
-			System.out.println(client.getStats());
-		};
-	}
-
-	@Bean
-	CommandLineRunner commandLineRunner() {
-		return args -> {
-			UserQLClient client1 = new UserQLClient();
-			client1.createUser();
-			client1.randomNumbers();
-			client1.getUsersSub();
-
-		};
-	}
+//	@Bean
+//	ApplicationRunner applicationRunner() {
+//		return args -> {
+//			UserRESTClient client = new UserRESTClient();
+//			client.createUser(new User(1L,"Alex","alex@gmail.com","pw","Medias"));
+//			client.getUserById(1L);
+//			client.getUserById(1L);
+//			System.out.println(client.getStats());
+//		};
+//	}
+//
+//	@Bean
+//	CommandLineRunner commandLineRunner() {
+//		return args -> {
+//			UserQLClient client1 = new UserQLClient();
+//			client1.createUser();
+//			client1.randomNumbers();
+//			client1.getUsersSub();
+//
+//		};
+//	}
 
 
 }
