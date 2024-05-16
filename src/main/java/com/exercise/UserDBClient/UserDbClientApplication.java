25f87fb0d4004c32e34dd73c9fbad5bfee765311
Alex Muresan <alexmuresan55@yahoo.com>
2024-05-16 14:43:48 +0300

    uncommented all in main

--- src/main/java/com/exercise/UserDBClient/UserDbClientApplication.java
+++ src/main/java/com/exercise/UserDBClient/UserDbClientApplication.java
@@ -1,8 +1,13 @@
 package com.exercise.UserDBClient;
 
+import com.exercise.UserDBClient.client.UserQLClient;
+import com.exercise.UserDBClient.client.UserRESTClient;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UserDbClientApplication {
@@ -11,27 +16,27 @@ public class UserDbClientApplication {
 		SpringApplication.run(UserDbClientApplication.class, args);
 	}
 
-//	@Bean
-//	ApplicationRunner applicationRunner() {
-//		return args -> {
-//			UserRESTClient client = new UserRESTClient();
-//			client.createUser(new User(1L,"Alex","alex@gmail.com","pw","Medias"));
-//			client.getUserById(1L);
-//			client.getUserById(1L);
-//			System.out.println(client.getStats());
-//		};
-//	}
+	@Bean
+	ApplicationRunner applicationRunner() {
+		return args -> {
+			UserRESTClient client = new UserRESTClient();
+			client.createUser(new User(1L,"Alex","alex@gmail.com","pw","Medias"));
+			client.getUserById(1L);
+			client.getUserById(1L);
+			System.out.println(client.getStats());
+		};
+	}
 
-//	@Bean
-//	CommandLineRunner commandLineRunner() {
-//		return args -> {
-//			UserQLClient client1 = new UserQLClient();
-//			client1.createUser();
-//			client1.randomNumbers();
-//			client1.getUsersSub();
-//
-//		};
-//	}
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			UserQLClient client1 = new UserQLClient();
+			client1.createUser();
+			client1.randomNumbers();
+			client1.getUsersSub();
+
+		};
+	}
 
 
 }
